9abb41e656b781785caf7263edbc5cdf869fd982
jimsa <cse.isalehi@gmail.com>
2022-11-14 17:08:31 +0330

    refactor naming

--- src/main/java/ir/jimsa/user/ws/security/SecurityConstants.java
+++ src/main/java/ir/jimsa/user/ws/security/SecurityConstants.java
@@ -3,9 +3,9 @@ package ir.jimsa.user.ws.security;
 public class SecurityConstants {
 
     public static final long EXPIRATION_TIME = 24 * 60 * 60 * 1000; // day, hour, minute, second, millisecond
-    public static final String TOKEN_PERFIX = "Bearer ";
+    public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization ";
     public static final String SIGN_UP_URL = "/users ";
-    public static final String TOKEN_SECRET = "Fgedfg4w5teavsDF4sdaf";
+    public static final String TOKEN_SECRET = "we3l4XyZUsqApFUJSnZfaVHZLp2BW8hFKHvdVHCSKecDHua4JrXN55Yxvt6mQXMB44l6VQP462Hd1cP1Oev5kG5Cc2LqDJi8zcOZcr7TVUZafaDdAyJvlHIo2jmvkE83fSwL0IlgiI9YYw49kpTX8B07HwieabNI5kTFOClxvpSP0lW7QPiBvpE8iQZfloyjLRGdW9QBQ9r4jsqNygAxUzafCpSK2I5q89oGQJGYUPYWq5NGOSYwX1oQWpLkN61H";
 
 }
