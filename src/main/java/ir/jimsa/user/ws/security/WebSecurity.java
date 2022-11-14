235ab1670547605e29b96c298349f40704ae8265
jimsa <cse.isalehi@gmail.com>
2022-11-14 12:29:02 +0330

    add web security

---
+++ src/main/java/ir/jimsa/user/ws/security/WebSecurity.java
@@ -0,0 +1,52 @@
+package ir.jimsa.user.ws.security;
+
+import ir.jimsa.user.ws.service.UserService;
+import ir.jimsa.user.ws.shared.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurity {
+
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public WebSecurity(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+        http
+//                .cors().and()
+                .csrf().disable()
+                .authorizeRequests()
+//                .antMatchers(HttpMethod.POST, Constant.USER_REGISTER_PATH).permitAll()
+                .antMatchers(Constants.USER_REGISTER_PATH).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .authenticationManager(authenticationManager)
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.headers().frameOptions().disable();
+
+        return http.build();
+
+    }
+
+}
