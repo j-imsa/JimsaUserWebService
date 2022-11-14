424ba080632c9b58576ef3d00ee7de7d5dac2be0
jimsa <cse.isalehi@gmail.com>
2022-11-14 17:11:06 +0330

    add SpringApplicationContext

--- src/main/java/ir/jimsa/user/ws/JimsaUserWebServiceApplication.java
+++ src/main/java/ir/jimsa/user/ws/JimsaUserWebServiceApplication.java
@@ -16,4 +16,9 @@ public class JimsaUserWebServiceApplication {
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SpringApplicationContext springApplicationContext() {
+        return new SpringApplicationContext();
+    }
 }
