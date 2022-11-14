74e89452edc94ed5bbf9525ad72d3348a018bc1a
jimsa <cse.isalehi@gmail.com>
2022-11-14 12:28:43 +0330

    add a path to registration

--- src/main/java/ir/jimsa/user/ws/shared/Constants.java
+++ src/main/java/ir/jimsa/user/ws/shared/Constants.java
@@ -1,5 +1,6 @@
 package ir.jimsa.user.ws.shared;
 
 public class Constants {
-    public static int USER_ID_LENGHT = 32;
+    public static int USER_ID_LENGTH = 32;
+    public static String USER_REGISTER_PATH = "/users";
 }
